78fa62806d9fcb71a31c5840db4391698e1a77b3
Tendai Nyandoro <tendai1@hotmail.com>
2021-03-01 00:06:39 +0200

    Add zert function

--- js/main.js
+++ js/main.js
@@ -2,24 +2,31 @@
 const time = document.getElementById('time'),
   greeting = document.getElementById('greeting'),
   name = document.getElementById('name'),
-  greeting = document.getElementById('');
+  focus = document.getElementById('focus');
 
   //  show Time
   function showTime() {
-    let today =new Date(),
-    hour = today.getHours(),
-    min = today.getMinutes(),
-    sec = today.getSeconds();
+    let today = new Date(),
+      hour = today.getHours(),
+      min = today.getMinutes(),
+      sec = today.getSeconds();
 
     // Set AM or PM
-    const amPm = hour >= 12 ? 'PM' : 'AM'
+    const amPm = hour >= 12 ? 'PM' : 'AM';
     // 12hr format
     hour = hour % 12 || 12;
 
     // Output Time
-    time.innerHTML = `${hour}<span>:</span>${min}<span>:</span>${sec}`;
+    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
 
     setTimeout(showTime, 1000);
+
+  }
+
+  // ADD ZERO FUNCTION
+  function addZero(n) {
+    return (parseInt(n, 10) < 10 ? '0' : '') + n;
+    
   }
 
   // Run
