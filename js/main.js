46018ce3ccd93b51818b358c87fe40d94c368359
Tendai Nyandoro <tendai1@hotmail.com>
2021-02-28 20:20:52 +0200

    Add the stylesheet

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Dom Elements 
+const time = document.getElementById('time'),
+  greeting = document.getElementById('greeting'),
+  name = document.getElementById('name'),
+  greeting = document.getElementById('');
+
+  //  show Time
+
+  function showTime() {
+    let today =new Date(),
+    hour = today.getHours(),
+    min = today.getMinutes(),
+    sec = today.getSeconds();
+
+    // Set AM or PM
+    const amPm = hour >= 12 ? 'PM' : 'AM'
+    // 12hr format
+    hour = hour % 12 || 12;
+
+    // Output Time
+  }
